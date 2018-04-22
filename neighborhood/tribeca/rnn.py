ce1aa64e05ecf87dac57fea9e6aae4c6d55de6ce
King Crawford <crawboy@me.com>
2018-04-22 17:43:36 -0400

    move plotting to its own function; add code for saving and loading model

--- neighborhood/tribeca/rnn.py
+++ neighborhood/tribeca/rnn.py
@@ -1,12 +1,35 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from keras.models import Sequential
+from keras.models import Sequential, model_from_json
 from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 import os
+
+
+def plot_first_week(input_data, look_back, train_predict, test_predict):
+    # shift train predictions for plotting
+    train_predict_plot = np.empty_like(input_data)
+    train_predict_plot[:, :] = np.nan
+    train_predict_plot[look_back:len(
+        train_predict) + look_back, :] = train_predict
+
+    # shift test predictions for plotting
+    test_predict_plot = np.empty_like(input_data)
+    test_predict_plot[:, :] = np.nan
+    test_predict_plot[len(train_predict) + (look_back * 2) +
+                      1:len(input_data) - 1, :] = test_predict
+
+    # plot baseline and predictions
+    days_of_week = ('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat')
+    plt.xlim(24, 192)
+    plt.xticks(np.arange(36, 204, step=24), days_of_week)
+    plt.plot(data)
+    plt.plot(train_predict_plot)
+    plt.plot(test_predict_plot)
+    plt.show()
 
 
 def create_dataset(dataset, look_back=1):
@@ -18,7 +41,7 @@ def create_dataset(dataset, look_back=1):
     return np.array(dataX), np.array(dataY)
 
 
-def do_stuff(input_data, look_back=1):
+def do_stuff(input_data, look_back=1, model=None):
     scaler = MinMaxScaler(feature_range=(0, 1))
     input_data = scaler.fit_transform(input_data)
 
@@ -32,11 +55,13 @@ def do_stuff(input_data, look_back=1):
     X_train = X_train.reshape((X_train.shape[0], 1, X_train.shape[1]))
     X_test = X_test.reshape((X_test.shape[0], 1, X_test.shape[1]))
 
-    model = Sequential()
-    model.add(LSTM(4, input_shape=(1, look_back)))
-    model.add(Dense(1))
+    if model is None:
+        model = Sequential()
+        model.add(LSTM(4, input_shape=(1, look_back)))
+        model.add(Dense(1))
+
     model.compile(loss='mean_squared_error', optimizer='adam')
-    hist = model.fit(X_train, y_train, epochs=5, batch_size=1, verbose=2)
+    model.fit(X_train, y_train, epochs=15, batch_size=1, verbose=2)
 
     # make predictions
     train_predict = model.predict(X_train)
@@ -49,51 +74,43 @@ def do_stuff(input_data, look_back=1):
     y_test = scaler.inverse_transform([y_test])
 
     # calculate root mean squared error
-    train_score = np.sqrt(mean_squared_error(
-        y_train[0], train_predict[:, 0]))
-    print('Train Score: {} RMSE'.format(train_score))
-    test_score = np.sqrt(mean_squared_error(y_test[0], test_predict[:, 0]))
-    print('Test Score: {} RMSE'.format(test_score))
+    # train_score = np.sqrt(mean_squared_error(
+    #     y_train[0], train_predict[:, 0]))
 
-    # shift train predictions for plotting
-    train_predict_plot = np.empty_like(input_data)
-    train_predict_plot[:, :] = np.nan
-    train_predict_plot[look_back:len(
-        train_predict) + look_back, :] = train_predict
-    # shift test predictions for plotting
-    test_predict_plot = np.empty_like(input_data)
-    test_predict_plot[:, :] = np.nan
-    test_predict_plot[len(train_predict) + (look_back * 2) +
-                      1:len(input_data) - 1, :] = test_predict
+    eps = 5e-9
+    train_score = 100 * np.mean(np.abs((
+        y_train[0] - train_predict[:, 0]) / (y_train[0] + eps)))
+    print('Train Score: {}% error'.format(train_score))
+    # test_score = np.sqrt(mean_squared_error(y_test[0], test_predict[:, 0]))
+    test_score = 100 * np.mean(np.abs((
+        y_test[0] - test_predict[:, 0]) / (y_test[0] + eps)))
+    print('Test Score: {}% error'.format(test_score))
+
+    # plot_first_week(input_data, look_back, train_predict, test_predict)
 
     return model
-
-def plot_first_week(month):
-    plot baseline and predictions
-    days_of_week = ('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat')
-    plt.xlim(24, 192)
-    plt.xticks(np.arange(36, 204, step=24), days_of_week)
-    plt.plot(data)
-    plt.plot(train_predict_plot)
-    plt.plot(test_predict_plot)
-    plt.show()
-
 
 # data = pd.read_csv('_30_minute_intersection.csv', usecols=[1],
 #                    sep=',').values.astype('float32').reshape(-1)
 
 numzones = 265
-for neighborhood in range(47, numzones + 1):
+for neighborhood in range(1, numzones + 1):
     data_dir = '../neighborhood' + str(neighborhood)
     print(data_dir)
 
     read = os.path.join(data_dir, 'ds-data-2017-05.csv')
     data = pd.read_csv(read, usecols=[1], sep=',').values.astype(
         'float32').reshape(-1, 1)
-    
-    model = do_stuff(data, look_back=1)
 
     save_location = os.path.join(data_dir, 'model')
+
+    with open(os.path.join(save_location,
+                           'model-architecture.json'), 'r') as m:
+        model = model_from_json(m.read())
+
+    model.load_weights(os.path.join(save_location, 'model-weights.h5'))
+    model = do_stuff(data, look_back=1, model=model)
+
     model.save_weights(os.path.join(save_location, 'model-weights.h5'))
     with open(
             os.path.join(save_location, 'model-architecture.json'), 'w'
