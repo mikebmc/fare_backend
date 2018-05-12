b6212bfe4f62025a28bec28497ff8e2c8c02d131
King Crawford <crawboy@me.com>
2018-05-12 16:21:25 -0400

    bug fixes

--- neighborhood/random/separate_neighborhoods_multivar.py
+++ neighborhood/random/separate_neighborhoods_multivar.py
@@ -12,7 +12,7 @@ read = os.path.join(data_dir, 'fixed_pickups_cut_yellow_tripdata_2016-05.csv')
 # delete files if they already exist
 neighborhoods = natsorted([dir for dir in os.listdir('../') if 'nei' in dir])
 for nbh in neighborhoods:
-    write = os.path.join('../', dir, 'data-{}.csv'.format(prd))
+    write = os.path.join('../', nbh, 'data-{}.csv'.format(prd))
     cleanup(write)
 
 
@@ -23,7 +23,7 @@ to_drop = ['month', 'weekday', 'Sunday', 'Monday', 'Tuesday',
 for count, chunk in enumerate(pd.read_csv(read, chunksize=10 ** 6), 1):
     chunk.drop(to_drop, axis=1, inplace=True)
 
-    groups = chunk.groupby('PULocationID')
+    groups = chunk.groupby('LocationID')
     for name, grp in groups:
         write = os.path.join(nbh_dir + str(name), 'data-{}.csv'.format(prd))
 
