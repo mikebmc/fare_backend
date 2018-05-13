8085b5ca04ad202d91343a28ddf03d06905eaae0
King Crawford <crawboy@me.com>
2018-05-13 15:41:51 -0400

    remove open(.) statement

--- neighborhood/random/mkprediction-multivar.py
+++ neighborhood/random/mkprediction-multivar.py
@@ -8,8 +8,9 @@ import io
 
 prd = '2017-05-multi'
 
+agg =[]
 neighborhoods = natsorted([dir for dir in os.listdir('../') if 'hood' in dir])
-for ii, nbh in enumerate(neighborhoods):
+for ii, nbh in enumerate(neighborhoods, 1):
     model_location = os.path.join('..', nbh, 'model-multi')
 
     with open(os.path.join(model_location, 'arc.json'), 'r') as m:
@@ -17,7 +18,4 @@ for ii, nbh in enumerate(neighborhoods):
     
     model.load_weights(os.path.join(model_location, 'weights.h5'))
 
-    s = io.StringIO()
-    with open(os.path.join('..', nbh, 'ds-data-{}.csv'.format(prd)), 'r') as d:
-        s.write(next(d))  # write header
-        s.write(next(line for num, line in enumerate(d) if num == itr))
+    # {'tpep_pickup_datetime': '2017-05-12 17:00:00', 'number_of_pickups': 376, 'PRCP': 0.4416, 'SNOW': 0, 'TMAX': 66.07, 'TMIN': 53.2}
